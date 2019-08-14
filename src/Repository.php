d68c6b94c99e6c747d361327fd86f3c654b91d5a
Esben Petersen <esbenspetersen@gmail.com>
2019-08-14 20:15:02 +0200

    soften type hint

--- src/Repository.php
+++ src/Repository.php
@@ -3,8 +3,8 @@
 namespace Optimus\Genie;
 
 use Illuminate\Database\DatabaseManager;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Query\Builder;
 use Optimus\Bruno\EloquentBuilderTrait;
 
 abstract class Repository
